b97b380748df8cbe3135a5e345b16e3d36cc3b4c
nakamura <kn_universe@yahoo.co.jp>
2019-06-18 12:04:33 +0000

    css調整 font-family設定

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -11,8 +11,8 @@
                             <span class="badge badge-pill badge-info offer-badge">オファーが来ています<span class="offer-badge__count badge-light badge-pill badge">{{ Auth::user()->is_offered_count() }}</span></span>
                         </div>
                     @endif
-                    <li class="nav-item">{!! link_to_route('logout.get','ログアウト', [], ['class' => 'nav-link nav-link text-white mt-1', 'id' => 'logout-button']) !!}</li>
-                    <li class="nav-item">{!! link_to_route('users.show','マイページ', ['id' => Auth::id()], ['class' => 'nav-link btn btn-lg btn-primary ml-3 text-white']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('logout.get','ログアウト', [], ['class' => 'nav-link nav-link text-white mt-2', 'id' => 'logout-button']) !!}</li>
+                    <li class="nav-item">{!! link_to_route('users.show','マイページ', ['id' => Auth::id()], ['class' => 'nav-link btn btn-lg btn-primary mt-1 ml-3 text-white']) !!}</li>
                 @else
                     <li class="nav-item">
                         {!! link_to_route('login','ログイン', [], ['class' => 'nav-link text-white mt-1']) !!}
